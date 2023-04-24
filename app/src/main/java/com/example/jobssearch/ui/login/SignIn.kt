47e13df2afe0d0cd016950f6f4c4cfd16c6eea56
Anjana <anjanadinuwan@gmail.com>
2023-04-24 11:39:32 +0530

    Testing button

--- app/src/main/java/com/example/jobssearch/ui/login/SignIn.kt
+++ app/src/main/java/com/example/jobssearch/ui/login/SignIn.kt
@@ -8,19 +8,35 @@ import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.View
 import android.view.inputmethod.EditorInfo
+import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import com.example.jobssearch.databinding.ActivitySignInBinding
 
 import com.example.jobssearch.R
+import kotlin.math.sign
 
 class SignIn : AppCompatActivity() {
-
+    var signInButton : Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+
+        signInButton = findViewById<Button>(R.id.btn_login)
+//        signInButton.text = "something"
+        signInButton!!.setOnClickListener { testButton() }
+
+        Log.d("Something", "here " )
+    }
+
+    fun testButton() {
+        Log.d("what", "testing")
+        signInButton!!.text = "something"
+
     }
 
 
