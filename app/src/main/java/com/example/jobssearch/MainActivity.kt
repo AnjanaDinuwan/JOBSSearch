90e2d50818da762ef8cd92b4c7f639962be06a4e
Anjana <anjanadinuwan@gmail.com>
2023-05-03 14:10:03 +0530

    Modified activity_main

--- app/src/main/java/com/example/jobssearch/MainActivity.kt
+++ app/src/main/java/com/example/jobssearch/MainActivity.kt
@@ -2,6 +2,11 @@ package com.example.jobssearch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomappbar.BottomAppBar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -11,3 +16,29 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+
+class RecommendedJobsAdapter(private val dataset: Array<String>) :
+        RecyclerView.Adapter<RecommendedJobsAdapter.ViewHolder>() {
+            class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+                init {
+
+                }
+            }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.job_card_layout, parent, false)
+
+        return ViewHolder(view);
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+        return dataset.size
+    }
+
+        }
