9db0cb75b931c1ec4ef2f9d9d7b030621f58f3ea
Anjana <anjanadinuwan@gmail.com>
2023-05-03 22:01:19 +0530

    Added a bunch of modifications to appearances

--- app/src/main/java/com/example/jobssearch/ProviderHome.kt
+++ app/src/main/java/com/example/jobssearch/ProviderHome.kt
@@ -1,16 +1,19 @@
 package com.example.jobssearch
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NavUtils
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import javax.xml.transform.ErrorListener
 
 
 class ProviderHome : AppCompatActivity() {
@@ -28,10 +31,15 @@ class ProviderHome : AppCompatActivity() {
         tList.add(2)
         tList.add(3)
         tList.add(4)
-        val jobAdapter = SeekerHome.JobAdapter(this, tList)
+        val jobAdapter = ProviderHome.CompanyAdapter(this, tList) { -> onCompanyCardClick() }
         val linearLayoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         recyclerView.layoutManager = linearLayoutManager
         recyclerView.adapter = jobAdapter
+    }
+
+    fun onCompanyCardClick() {
+        val intent = Intent(this, CompanyDetails::class.java)
+        startActivity(intent)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -52,13 +60,15 @@ class ProviderHome : AppCompatActivity() {
         }
     }
 
-    class JobAdapter(private val context: Context, private val jobArrayList: ArrayList<Int>) :
-        RecyclerView.Adapter<JobAdapter.ViewHolder>() {
+    class CompanyAdapter(private val context: Context, private val jobArrayList: ArrayList<Int>, private val listener: () -> Unit) :
+        RecyclerView.Adapter<CompanyAdapter.ViewHolder>() {
 
         class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+            var companyCard: RelativeLayout
             init {
-
+                companyCard = view.findViewById<RelativeLayout>(R.id.company_card)
             }
+
         }
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -69,7 +79,9 @@ class ProviderHome : AppCompatActivity() {
         }
 
         override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-
+            holder.companyCard.setOnClickListener{
+                listener()
+            }
         }
 
         override fun getItemCount() = jobArrayList.size
