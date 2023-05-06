955d648e7f219fb18f54d71fdb1b2b248a56ab0a
Anjana <anjanadinuwan@gmail.com>
2023-05-06 15:04:10 +0530

    change app icons and remove unwanted drawables

--- app/src/main/java/com/example/jobssearch/UpdatePosition.kt
+++ app/src/main/java/com/example/jobssearch/UpdatePosition.kt
@@ -7,5 +7,10 @@ class UpdatePosition : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_update_position)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setTitle(R.string.UpdatePosition_title)
+        supportActionBar?.elevation = 0.0F
+
     }
 }
